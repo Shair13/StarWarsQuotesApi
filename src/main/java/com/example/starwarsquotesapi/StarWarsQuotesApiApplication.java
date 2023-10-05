dd24c8eea5912d4193de15e61a5822fd59fa26b7
Szer13 <shad350@gmail.com>
2023-10-05 23:18:59 +0200

    first commit

---
+++ src/main/java/com/example/starwarsquotesapi/StarWarsQuotesApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.starwarsquotesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsQuotesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarWarsQuotesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/starwarsquotesapi/StarWarsQuotesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.starwarsquotesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarWarsQuotesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
