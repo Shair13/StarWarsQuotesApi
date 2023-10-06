6c4cfee53b0bb183b0d0eba5956fd1c133a5f0e3
Szer13 <shad350@gmail.com>
2023-10-06 10:44:18 +0200

    Quote class

--- src/main/java/com/example/starwarsquotesapi/model/Quote.java
+++ src/main/java/com/example/starwarsquotesapi/model/Quote.java
@@ -1,4 +1,30 @@
 package com.example.starwarsquotesapi.model;
 
 public class Quote {
+    private String content;
+    private String author;
+
+    public Quote() {
+    }
+
+    public Quote(String content, String author) {
+        this.content = content;
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 }
