b090c136e642d951ef0de6cb6da575d70cddd161
Szer13 <shad350@gmail.com>
2023-10-06 13:05:30 +0200

    searching by character

--- src/main/java/com/example/starwarsquotesapi/repository/QuoteRepository.java
+++ src/main/java/com/example/starwarsquotesapi/repository/QuoteRepository.java
@@ -3,8 +3,10 @@ package com.example.starwarsquotesapi.repository;
 import com.example.starwarsquotesapi.model.Quote;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuoteRepository extends JpaRepository<Quote, Long> {
 
-
+List<Quote> findAllByAuthor(String author);
 
 }
